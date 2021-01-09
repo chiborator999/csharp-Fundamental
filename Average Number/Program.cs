a41496b9099a4bd1319061e21d81cd9ff5311ce7
chiborator999 <dsspasov@gmail.com>
2021-01-09 21:05:36 +0200

    add file to Fundamental

---
+++ Average Number/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Average_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            double sum = 0;
+            double avergeSum = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                sum += num;
+                avergeSum = sum / n;
+            }
+            Console.WriteLine($"{avergeSum:F2}");
+        }
+    }
+}
